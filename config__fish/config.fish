a6b920bc3f2467368f1c50e493cdcb6d617afca4
Thomas Stromberg <tstromberg@google.com>
2018-09-26 14:12:02 -0700

    Add git->hub alias.

--- config__fish/config.fish
+++ config__fish/config.fish
@@ -2,5 +2,10 @@
 if test -d /google
   source /google/data/ro/teams/fish/google.fish
 end
+
+if test -f $HOME/go/bin/hub
+  alias git="$HOME/go/bin/hub"
+end
+
 set -x CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES "1600x1200,3840x2560"
 set --universal fish_user_paths $HOME/bin $HOME/go/bin $fish_user_paths
