e110c0edcecdc978848ec202804f4613c6227813
Thomas Stromberg <tstromberg@google.com>
2018-09-26 18:38:56 -0700

    Cast a wider net

--- config__fish/functions/r.fish
+++ config__fish/functions/r.fish
@@ -1,3 +1,8 @@
-function r
-  cd (find ~/go/src/github.com -maxdepth 2 -name $argv | head -n 1)
+function r -d "repo switcher" -a name
+  set repo (find ~/go/src ~/src -maxdepth 2 -name $name | head -n 1)
+  if test -n $repo
+    cd $repo
+  else
+    echo "Unable to find repo for $name"
+  end
 end
