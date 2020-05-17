c12e996554ceb5eea7f23f9a4920ff8748de1474
Thomas Stromberg <thomas@stromberg.org>
2020-05-17 13:46:37 -0700

    Improve bourne-shell compat

--- config__fish/config.fish
+++ config__fish/config.fish
@@ -1,3 +1,1 @@
-# From http://go/fish-shell
-source /google/data/ro/teams/fish/google.fish
-set -x CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES "1600x1200,3840x2560"
+set -gx PATH /usr/pkg/bin /usr/pkg/sbin /sbin $PATH
