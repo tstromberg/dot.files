bff191085e5bf772916b0e3ae894a9347a5dc30a
Thomas Stromberg <tstromberg@google.com>
2020-12-05 09:26:25 -0800

    Changes from the macbook

--- config__fish/config.fish
+++ config__fish/config.fish
@@ -1,10 +1,7 @@
 # From http://go/fish-shell
-if test -d /google
-  source /google/data/ro/teams/fish/google.fish
-end
-
 # status -i; and /home/build/google3/experimental/users/tstromberg/minikube/tunnel.sh
-
 set -x CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES "1600x1200,3840x2560"
-set --universal fish_user_paths $HOME/bin $HOME/go/bin $fish_user_paths /usr/local/go/bin
+set --universal fish_user_paths $HOME/bin $HOME/go/bin $fish_user_paths /usr/local/go/bin /usr/local/fluter/bin
+source $HOME/google-cloud-sdk/path.fish.inc
 
+set -g fish_user_paths "/usr/local/bin" "/usr/local/opt/openjdk/bin" $fish_user_paths
