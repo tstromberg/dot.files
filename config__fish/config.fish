f8846fe504fbc851aa16219c15b09f4043de8bc0
Thomas Stromberg <tstromberg@google.com>
2020-12-05 09:27:49 -0800

    Remove tag

--- config__fish/config.fish
+++ config__fish/config.fish
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # From http://go/fish-shell
 # status -i; and /home/build/google3/experimental/users/tstromberg/minikube/tunnel.sh
 set -x CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES "1600x1200,3840x2560"
