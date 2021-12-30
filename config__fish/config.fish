fee285d036a8a3d79671c886f518ccac3a6f6ace
Thomas Stromberg <t+github@stromberg.org>
2021-12-30 14:51:21 -0500

    Add wotd, fix paths

--- config__fish/config.fish
+++ config__fish/config.fish
@@ -1,9 +1,8 @@
 # From http://go/fish-shell
 set -x CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES "1600x1200,3840x2560"
-set --universal fish_user_paths $HOME/bin $HOME/go/bin $fish_user_paths /usr/local/go/bin /usr/local/flutter/bin
-# source $HOME/google-cloud-sdk/path.fish.inc
-set -g fish_user_paths "/usr/local/bin" "/usr/local/opt/openjdk/bin" $fish_user_paths
-set -gx PATH /usr/pkg/bin /usr/pkg/sbin /sbin $PATH
+set -gx PATH $HOME/bin $HOME/go/bin $fish_user_paths /usr/local/go/bin /usr/pkg/bin /usr/pkg/sbin /sbin $PATH
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/t/google-cloud-sdk/path.fish.inc' ]; . '/Users/t/google-cloud-sdk/path.fish.inc'; end
+if status --is-login
+  wotd
+end
+
