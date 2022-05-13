4efc5453467f7fcb63b1474419d9104c8eb9a36e
Thomas Stromberg <t+github@stromberg.org>
2022-05-12 18:09:25 -0700

    Add comments, Google Home

--- minimal_android_phone.sh
+++ minimal_android_phone.sh
@@ -8,21 +8,33 @@ for pkg in $(adb shell pm list packages | sed s/package://g | egrep "\.(chromium
   adb shell pm uninstall --user 0 $pkg
 done
 
-# Install 3rd party apps
+# Install 3rd party apps with a low likelihood of distraction
+#
+#   - Tesla
+#   - olauncher (minimal replacement for the Android launcher)
+#   - Signal
+#   - Garmin Connect (watch)
+#   - Fuji Camera Remote
+#   - Google Keep
+#   - Google Home
 apps="https://storage.evozi.com/apk/dl/16/09/08/com.teslamotors.tesla_443.apk
 https://storage.evozi.com/apk/dl/20/08/17/app.olauncher_33.apk
 https://updates.signal.org/android/Signal-Android-website-prod-universal-release-5.36.3.apk
 https://storage.evozi.com/apk/dl/16/09/04/com.garmin.android.apps.connectmobile_5519.apk
 https://storage.evozi.com/apk/dl/16/09/07/com.fujifilm_dsc.app.remoteshooter_29.apk
-https://storage.evozi.com/zip/dl/16/09/04/com.google.android.apps.chromecast.app_0.zip
-https://storage.evozi.com/apk/dl/16/09/04/com.google.android.keep_214810140.apk"
+https://storage.evozi.com/apk/dl/16/09/04/com.google.android.keep_214810140.apk
+https://www.apkmirror.com/wp-content/uploads/2021/12/65/61af968ea9b62/com.google.android.apps.chromecast.app_2.47.1.10-24701100_minAPI23(arm64-v8a,armeabi-v7a,x86,x86_64)(nodpi)_apkmirror.com.apk?verify=1652403422-H1no4xuB1mWvwHcIrJLQj5GB7BiBw9yHOm39H6J8O4U
+"
 
 for a in $apps; do
-  local=$(basename $a)
+  local="$(basename $(echo $a | cut -d? -f1))"
+
+  pkg=$(echo $local | cut -d_ -f1)
+  adb shell pm list packages | grep ${pkg} && continue
 
   if [ ! -f "${local}" ]; then
-    curl -LO $a
+    curl -L -o "${local}" "${a}"
   fi
-  
+
   adb install $local
 done
