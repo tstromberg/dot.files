2772d63ca343e0203c669c4f418edb985246526e
Thomas Stromberg <t+github@stromberg.org>
2022-05-12 18:20:23 -0700

    Add docs

--- minimal_android_phone.sh
+++ minimal_android_phone.sh
@@ -1,9 +1,40 @@
 #!/bin/sh
-# Post-GrapheneOS setup steps
+# 
+# Distraction-free Android installer
+#
+# This script will disable most sources of distractions on your phone. It requires
+# USB debugging (Developer Mode) to be enabled, and is designed to be run right after
+# a factory reset. It can also be run at any point afterwards.
+#
+# Think of it as a "LightPhone"-like experience on a stock-android phone
+#
+# What's it disable?
+#
+#   - Google Chrome / Chromium Web Browsers (but not the webview)
+#   - Google Play Store
+#   - Google Maps
+#   - Verizon apps
+#   - Wellbeing
+#   - Audio Recorder
+#   - Files
+#   - Youtube Music
+#   - Youtube
+#   - Google Search Box
+#   - Google Docs
+#
+# Recommended post-execution steps:
+#
+#   - Replace the Android launcher with olauncher
+#   - Change your interface language to one you vaguely understand or want to learn more of
+#   - Force your screen into monochrome mode (Settings -> Developer -> Simulate color space -> Monochromacy)
+# 
+readonly DISABLE_RE="\.(chromium|chrome|verizon|vending|wellbeing|recorder|files|gm|tips|youtube|music|amplifier|gmm|maps|googlequicksearchbox|docs)\$"
 
+# Convenience for an extra path one might happen to have 'adb' installed.
 export PATH=$PATH:$HOME/Downloads/platform-tools
 
-for pkg in $(adb shell pm list packages | sed s/package://g | egrep "\.(chromium|chrome|verizon|vending|wellbeing|recorder|files|gm|tips|youtube|music|amplifier|gmm|maps|googlequicksearchbox|docs)\$"); do
+# Disable and uninstall apps for user 0 (default)
+for pkg in $(adb shell pm list packages | sed s/package://g | egrep "${DISABLE_RE}"); do
   adb shell pm disable-user --user 0 $pkg
   adb shell pm uninstall --user 0 $pkg
 done
@@ -11,12 +42,15 @@ done
 # Install 3rd party apps with a low likelihood of distraction
 #
 #   - Tesla
-#   - olauncher (minimal replacement for the Android launcher)
+#   - olauncher (to replace the built-in Android launcher)
 #   - Signal
 #   - Garmin Connect (watch)
 #   - Fuji Camera Remote
 #   - Google Keep
 #   - Google Home
+#
+# Mostly sourced through https://apps.evozi.com/apk-downloader/ (no guarantee against malware)
+#
 apps="https://storage.evozi.com/apk/dl/16/09/08/com.teslamotors.tesla_443.apk
 https://storage.evozi.com/apk/dl/20/08/17/app.olauncher_33.apk
 https://updates.signal.org/android/Signal-Android-website-prod-universal-release-5.36.3.apk
